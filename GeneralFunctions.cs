b2c7b008151a678d7bbbbf184d176facbbd4c85d
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2024-02-27 17:29:52 -0700

    Started General Code Setup ; returnRandInt()

---
+++ GeneralFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApp_SCRUM
+{
+    internal class GeneralFunctions
+    {
+
+        // Input Min and Max Number Range to Return Random Number within that Range
+        public int returnRandInt(int min, int max)
+        {
+            if (min < 0 || max < 0 || min >= max) // Returns 0 if number input is below 0 or if the two numbers are equal to each other
+            {
+                return 0;
+            }
+            Random random = new Random();
+            return random.Next(min, max + 1); // +1 includes the max number in the possible outcomes
+        }
+
+    }
+}
